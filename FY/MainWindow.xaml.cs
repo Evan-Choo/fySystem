ed2a375dd1024263d3a48133b69102067602628f
Evan <Evan@DESKTOP-DJPOQRE>
2018-09-19 15:43:49 +0800

    FYjobs

--- FY/MainWindow.xaml.cs
+++ FY/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace FY
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string filepath = "c:\\test.txt";
+        private string fyjobspath = "c:\\test.txt";
 
         public MainWindow()
         {
@@ -33,7 +33,7 @@ namespace FY
         {
             int totalTasks = 0;
             
-            FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            FileStream fs = new FileStream(fyjobspath, FileMode.Open, FileAccess.Read);
             StreamReader sr = new StreamReader(fs);
 
             //去除第一行的表头
@@ -41,6 +41,120 @@ namespace FY
             while ((s = sr.ReadLine())!= null){
                 totalTasks++;
             }
+
+            Console.WriteLine(totalTasks);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int runningTasks = 0;
+
+            FileStream fs = new FileStream(fyjobspath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            //去除表头
+            string s = sr.ReadLine();
+            while((s=sr.ReadLine())!=null)
+            {
+                string[] splits = s.Split(' ');
+                ArrayList al = new ArrayList();
+
+                foreach(string a in splits)
+                {
+                    if(a.Length!=0)
+                        al.Add(a);
+                }
+
+                string[] strings = (string[])al.ToArray(typeof(string));
+                if (strings[1].Equals("RUN"))
+                    runningTasks++;
+            }
+
+            Console.WriteLine(runningTasks);
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            int pendingTasks = 0;
+
+            FileStream fs = new FileStream(fyjobspath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            //去除表头
+            string s = sr.ReadLine();
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] splits = s.Split(' ');
+                ArrayList al = new ArrayList();
+
+                foreach (string a in splits)
+                {
+                    if (a.Length != 0)
+                        al.Add(a);
+                }
+
+                string[] strings = (string[])al.ToArray(typeof(string));
+                if (strings[1].Equals("PEND"))
+                    pendingTasks++;
+            }
+
+            Console.WriteLine(pendingTasks);
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            int PGSs = 0;
+
+            FileStream fs = new FileStream(fyjobspath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            //去除表头
+            string s = sr.ReadLine();
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] splits = s.Split(' ');
+                ArrayList al = new ArrayList();
+
+                foreach (string a in splits)
+                {
+                    if (a.Length != 0)
+                        al.Add(a);
+                }
+
+                string[] strings = (string[])al.ToArray(typeof(string));
+                if (strings[3].Contains("PGS"))
+                    PGSs++;
+            }
+
+            Console.WriteLine(PGSs);
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            int DSSs = 0;
+
+            FileStream fs = new FileStream(fyjobspath, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            //去除表头
+            string s = sr.ReadLine();
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] splits = s.Split(' ');
+                ArrayList al = new ArrayList();
+
+                foreach (string a in splits)
+                {
+                    if (a.Length != 0)
+                        al.Add(a);
+                }
+
+                string[] strings = (string[])al.ToArray(typeof(string));
+                if (strings[3].Contains("DSS"))
+                    DSSs++;
+            }
+
+            Console.WriteLine(DSSs);
         }
     }
 }
